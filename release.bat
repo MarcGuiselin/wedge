a03d34f7098213361280a276f729a3cac2c0f68d
Marc Guiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-11-21 13:17:25 +0100

    Generate checksums file

--- release.bat
+++ release.bat
@@ -3,3 +3,9 @@ cargo +nightly fmt
 cargo build --release --workspace --exclude installer
 :: Now build installer
 cargo build --release --package installer
+:: Generate checksum file
+@echo off
+for /f "skip=1" %%a in (
+  'certutil -hashfile ./target/release/installer.exe SHA256'
+) do if not defined sha set "sha=%%a"
+echo %sha% installer.exe > ./target/release/checksums.txt
