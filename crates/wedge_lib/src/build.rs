a9f8c336f76a13b5a1f9591585dd66df2b3f0c41
Marc Guiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-11-23 13:55:00 +0100

    Roll back deps and lockfile to allow older toolchains

--- crates/wedge_lib/src/build.rs
+++ crates/wedge_lib/src/build.rs
@@ -42,7 +42,7 @@ fn get_package_details() -> (String, String) {
     }
 
     // Get workspace root
-    let ret2 = String::from(metadata.workspace_root.as_str());
+    let ret2 = String::from((&metadata.workspace_root).to_str().unwrap());
     (ret.unwrap(), ret2)
 }
 
